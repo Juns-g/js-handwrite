f4ecc7dcadbf77687636a4bd8fb1bddb3fa6c07b
Juns <juns.g@foxmail.com>
2024-02-17 14:53:42 +0800

    feat: new

--- src/new.js
+++ src/new.js
@@ -16,3 +16,15 @@ function myNew(fn, ...args) {
 	const val = fn.apply(newObj, args);
 	return val instanceof Object ? val : newObj;
 }
+
+function myNew_2(ctor, ...args) {
+	if (typeof ctor !== 'function') {
+		throw new Error('构造函数必须是function');
+	}
+	const obj = {};
+	// 原型链链接有很多方案
+	obj.__proto__ = ctor.prototype;
+	const res = ctor.apply(obj, args);
+	// 如果构造函数返回对象，就return出去
+	return res instanceof Object ? res : obj;
+}
