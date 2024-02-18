d6817f00fbd9583671ce6a34a617828ec6ddf0aa
Juns <juns.g@foxmail.com>
2024-02-18 19:59:33 +0800

    feat: update

--- temp/temp.js
+++ temp/temp.js
@@ -1,7 +1,46 @@
-let str = `
-1 2 4342    3 4 5
-23 4
-    4  5
-`;
+class Scheduler {
+	constructor(count = 2) {
+		this.maxCount = count;
+		this.runCount = 0;
+		this.queue = [];
+	}
+	add(promiseCreator) {
+		return new Promise((resolve) => {
+			this.queue.push(() => promiseCreator().then(resolve));
+			this.run();
+		});
+	}
+	run() {
+		if (this.runCount >= this.maxCount || !this.queue.length) return;
+		const task = this.queue.shift();
+		this.runCount++;
+		task().then(() => {
+			this.runCount--;
+			this.run();
+		});
+	}
+}
 
-let s = str.trim();
+const scheduler = new Scheduler();
+
+function timeout(time) {
+	return () =>
+		new Promise((resolve) => {
+			setTimeout(() => {
+				resolve();
+			}, time);
+		});
+}
+
+function addTask(time, order) {
+	scheduler.add(timeout(time)).then(() => {
+		console.log(order);
+	});
+}
+
+addTask(1000, '1');
+addTask(500, '2');
+addTask(300, '3');
+addTask(400, '4');
+
+// Scheduler最多支持两个正在处理的promise，最终打印顺序是2 3 1 4
