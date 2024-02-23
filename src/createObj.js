852baba2b0274d462c9b59f76fe6893b1e440d73
Juns <juns.g@foxmail.com>
2024-02-23 21:08:59 +0800

    style: 更改变量名

--- src/createObj.js
+++ src/createObj.js
@@ -1,15 +1,16 @@
 // 给定形如 'a.b.c.d' 的字符串，构造一个对象如下: { a:{ b:{ c:{ d:null } } } }
+// ! 字节考过，2024.2.23 商业化三面
 
 function createObj(path) {
 	const keys = path.split('.');
 	let res = {};
-	let temp = res;
+	let cur = res;
 	keys.forEach((key) => {
 		if (key === keys.at(-1)) {
-			temp[key] = null;
+			cur[key] = null;
 		} else {
-			temp[key] = {};
-			temp = temp[key];
+			cur[key] = {};
+			cur = cur[key];
 		}
 	});
 	return res;
