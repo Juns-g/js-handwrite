def4cf4ab98934edf76875eefa218e13606659dd
bianrenping <bianrenping@bytedance.com>
2024-08-09 16:15:55 +0800

    feat: arr2Tree

--- temp/temp.js
+++ temp/temp.js
@@ -1,33 +1,41 @@
-function debounce(fn, wait = 500, immediate) {
-	let timer = null
+const data = [
+	{ id: 12, parentId: 1, name: '朝阳区' },
+	{ id: 241, parentId: 24, name: '田林街道' },
+	{ id: 31, parentId: 3, name: '广州市' },
+	{ id: 13, parentId: 1, name: '昌平区' },
+	{ id: 2421, parentId: 242, name: '上海科技绿洲' },
+	{ id: 21, parentId: 2, name: '静安区' },
+	{ id: 242, parentId: 24, name: '漕河泾街道' },
+	{ id: 22, parentId: 2, name: '黄浦区' },
+	{ id: 11, parentId: 1, name: '顺义区' },
+	{ id: 2, parentId: 0, name: '上海市' },
+	{ id: 24, parentId: 2, name: '徐汇区' },
+	{ id: 1, parentId: 0, name: '北京市' },
+	{ id: 2422, parentId: 242, name: '漕河泾开发区' },
+	{ id: 32, parentId: 3, name: '深圳市' },
+	{ id: 33, parentId: 3, name: '东莞市' },
+	{ id: 3, parentId: 0, name: '广东省' },
+	{ id: 0, parentId: null, name: '广东省' },
+]
 
-	return function (...args) {
-		const context = this
-
-		if (immediate && !timer) {
-			fn.apply(context, args)
+const arr2Tree = (data) => {
+	const map = new Map()
+	for (let item of data) map.set(item.id, item)
+	let tree = {}
+	for (let item of data) {
+		const node = map.get(item.id)
+		if (!node.parentId) {
+			tree = node
+			continue
 		}
-
-		clearTimeout(timer)
-		timer = setTimeout(() => {
-			if (!immediate) {
-				fn.apply(context, args)
-			}
-			timer = null // 重置 timer
-		}, wait)
+		const parentNode = map.get(node.parentId)
+		if (!parentNode?.children) {
+			parentNode.children = [node]
+		} else {
+			parentNode.children.push(node)
+		}
 	}
+	return tree
 }
 
-const newFn = debounce(
-	() => {
-		console.log(111)
-	},
-	1000,
-	true,
-)
-
-newFn()
-newFn()
-newFn()
-newFn()
-newFn()
+console.log(arr2Tree(data))
