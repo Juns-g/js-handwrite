fb14decc77f9774d9971396641f9009d3be77f21
Juns <juns.g@foxmail.com>
2024-09-26 15:29:15 +0800

    feat: update

--- temp/temp2.js
+++ temp/temp2.js
@@ -1,35 +1,89 @@
-class TemplateEngine {
-	constructor(template, data) {
-		this.template = template
-		this.data = data
+class Man {
+	constructor(name) {
+		this.name = name
+		this.queue = []
+		this.sayHi()
 	}
-
-	render() {
-		let output = this.template
-
-		// 正则表达式匹配模板中的变量
-		const regex = /{{\s*([^}\s]+)\s*}}/g
-
-		// 匹配模板中的变量，并用数据中对应的值替换
-		output = output.replace(regex, (match, variable) => {
-			let value = this.data
-
-			// 处理多层级属性访问
-			variable.split('.').forEach((prop) => {
-				value = value[prop]
-			})
-
-			return value
+	sayHi() {
+		this.queue.push(() => {
+			console.log(`Hi!this is ${this.name}`)
+			this.next()
 		})
-
-		return output
+	}
+	sleep(time) {
+		this.queue.push(() => {
+			setTimeout((params) => {
+				console.log('wake up after' + time)
+				this.next()
+			}, time * 1000)
+		})
+		return this
+	}
+	sleepFirst(time) {
+		this.queue.unshift(() => {
+			setTimeout(() => {
+				console.log(`等待${time}秒`)
+				this.next()
+			}, time * 1000)
+		})
+		return this
+	}
+	eat() {
+		this.queue.push(() => {
+			console.log('eat')
+			this.next()
+		})
+	}
+	next() {
+		const task = this.queue.shift()
+		task && task()
 	}
 }
 
-// 示例模板
-const template = '<h1>{{ title }}</h1><p>{{ content.text }}</p>'
-const data = { title: 'Hello', content: { text: 'World' } }
+new Man('Hank').sleep(2).sleepFirst(3).eat('supper')
 
-const engine = new TemplateEngine(template, data)
-const rendered = engine.render()
-console.log(rendered)
+function fun(e) {
+	this.e = e
+	this.queue = []
+	this.sayHi = function () {
+		this.queue.push(() => {
+			console.log(`Hi!this is ${this.e}`)
+			this.next()
+		})
+	}
+	this.eat = function (e) {
+		this.queue.push(() => {
+			console.log(`Eat ${e}~`)
+		})
+		return this
+	}
+	this.sleep = function (num) {
+		this.queue.push(() => {
+			setTimeout(() => {
+				console.log(`Wake up after ${num}`)
+				this.next()
+			}, num * 1000)
+		})
+		return this
+	}
+	this.sleepFirst = function (num) {
+		this.queue.unshift(() => {
+			setTimeout(() => {
+				console.log(`等待${num}秒`)
+				this.next()
+			}, num * 1000)
+		})
+		return this
+	}
+	this.next = function () {
+		let fn = this.queue.shift()
+		fn && fn()
+	}
+	this.sayHi()
+	setTimeout(() => {
+		this.next()
+	})
+	return this
+}
+
+new fun('Hank').sleep(2).sleepFirst(3).eat('supper')
